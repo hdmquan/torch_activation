3857b167f332aa37772bb016ea143833737e8503
AnnNguyen975 <phuonganh.090705@gmail.com>
2025-03-07 07:35:38 +0700

    feat: Add Sine

--- torch_activation/classical/other.py
+++ torch_activation/classical/other.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch import Tensor
+import math
 
 from torch_activation import register_activation
 
@@ -33,4 +34,29 @@ class Binary(nn.Module):
         else:
             return torch.where(z < 0, torch.zeros_like(z), torch.ones_like(z))
     
+
+@register_activation
+class Sine(nn.Module):
+    r"""
+    Applies the Binary activation function:
+
+    :math:`\text{Binary}(z) = \begin{cases} 
+    0, & z < 0 \\
+    1, & z \geq 0 
+    \end{cases}`
+
+    Args:
+        inplace (bool, optional): can optionally do the operation in-place. Default: ``False``
+
+    Shape:
+        - Input: :math:`(*)`, where :math:`*` means any number of dimensions.
+        - Output: :math:`(*)`, same shape as the input.
+    """
+
+    def __init__(self, inplace: bool = False):
+        super(Binary, self).__init__()
+        self.inplace = inplace
+
+    def forward(self, z) -> Tensor:
+        return torch.sin(math.pi * z)
     
